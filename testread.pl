675b6eb2d24acaec1b4a13cf54ec971d3dc45e4c
wrosner <wrosner@fuji1.rosner.lokal>
2020-12-18 23:47:34 +0100

    boiler plate state machine

--- testread.pl
+++ testread.pl
@@ -13,15 +13,30 @@ binmode DATAIN;
 my $data;
 my $nbytes;
 
+my $status =0;
+
 while ($nbytes = read DATAIN, $data, 1) {
 
   # do stuff
-  print ".";
+  # print ".";
   $byte = ord ($data);
   $hex = sprintf ("%02x", $byte);
-  $ihex = sprintf ("%02x", $byte ^ hex('ff'));
-  print "\n" if ( $ihex eq '24');
-  printf ("%d:%s-%s|", $nbytes, $hex, $ihex );
+  # $ihex = sprintf ("%02x", $byte ^ hex('ff'));
+  # print "\n" if ( $hex eq '24');
+  # printf (" %s", $hex );
+
+  # do the state machine
+  if ( $hex eq '24') {
+    $status++;
+    print "\n" if ($status == 1) ;
+    if ($status > 2) {
+	print "\t.oopsie - too many 24'rs \n";
+	$status = 0;
+    }
+  } else {
+    $status = 0;
+  } 
+  printf (" %s", $hex );
 
 
 }
