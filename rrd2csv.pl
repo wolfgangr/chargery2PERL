76a854a8604edeceab657209cb5eaea86b12d48a
wrosner <wrosner@kellerkind.rosner.lokal>
2020-12-22 17:55:07 +0100

    pass resolution and align options

--- rrd2csv.pl
+++ rrd2csv.pl
@@ -12,22 +12,28 @@ use DateTime;
 
 
 # we need at least a rrd file name and a CF
-die "usage $0 db.rrd CF [-s start] [-e end] [-h] [-x sep] [-d delim] " unless $#ARGV >= 1;
+die "usage $0 db.rrd CF [-s start] [-e end] [-r res] [-a] [-h] [-x sep] [-d delim] " unless $#ARGV >= 1;
 my $rrdfile = shift @ARGV;
 my $cf      = shift @ARGV;
 
-getopts('s:e:hx:d:');
+getopts('s:e:hx:d:r:a');
 
 $start  = $opt_s || 'e-1d';
 $end    = $opt_e || 'N';
 $header = $opt_h;
 $sep    = $opt_x;
 $delim  = $opt_d;
+$align  = $opt_a;
+$res    = $opt_r;
 
-printf STDERR "parameter db=%s CF=%s start=%s end=%s header=%s sep=%s delim=%s \n",
-	$rrdfile, $cf, $start, $end, $header , $sep, $delim      ;
+printf STDERR "parameter db=%s CF=%s start=%s end=%s resolution=%s align=%d header=%s sep=%s delim=%s \n",
+	$rrdfile, $cf, $start, $end, $res, $align, $header , $sep, $delim      ;
 
 @paramlist = ($rrdfile, $cf, '-s', $start, '-e', $end);
+push @paramlist, '-a' if $align ;
+push @paramlist, ('-r', $res ) if $res ; 
+
+print STDERR join (@paramlist, ' | '), "\n";
 
 my ($start,$step,$names,$data) = RRDs::fetch (@paramlist);
 
