8b6a3bbd156a23df3e23d2ceae239365452d2cbf
wrosner <wrosner@kellerkind.rosner.lokal>
2020-12-23 18:23:06 +0100

    debugging database sync

--- sync-chargery-rrd-to-SQL.sh
+++ sync-chargery-rrd-to-SQL.sh
@@ -12,20 +12,24 @@ cd $SCRIPTDIR
 for TAG in cells pack56 pack57 
   do
 	TABLENAME=rrd_upload_${TAG}
-	TEMPFILE=${TMPDIR}\${TABLENAME}.csv
+	TEMPFILE=${TMPDIR}/${TABLENAME}.csv
 	RRDFILE=${TAG}.rrd
 	# ./rrd2csv.pl pack56.rrd AVERAGE -r 300 -x\; -M -t -f maria1-csv
+
+	echo $RRDFILE ' -> ' $TEMPFILE
+	# echo $TEMPFILE
+	# echo ${TABLENAME}
+	# exit
 
 	./rrd2csv.pl $RRDFILE AVERAGE -r 300 -x\; -M -t -f $TEMPFILE
 
 	# mysqlimport -h homeserver -u solarlog-writer -pfoo --local --force  
 	#	--ignore-lines=1   --fields-terminated-by=';'   -d   chargery 'maria1.csv'
 
-  	mysqlimport-h $HOST -u $USER -p$PASSWD  --local \
+  	mysqlimport -h $HOST -u $USER -p$PASSWD  --local \
 		--replace --force \
 		--ignore-lines=1 --fields-terminated-by=';' \
 		chargery $TMPFILE
 
   done
 
-# mysqlimport -h homeserver -u solarlog-writer -pfoo --local --force   --ignore-lines=1   --fields-terminated-by=';'   -d   chargery 'maria1.csv'
