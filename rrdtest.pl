62c8481e7c0d3e9a01852101c0af39d92ccbb0b4
wrosner <wrosner@kellerkind.rosner.lokal>
2020-12-23 10:09:20 +0100

    rearrange

--- rrdtest.pl
+++ rrdtest.pl
@@ -35,10 +35,37 @@ foreach $arg (@ARGV ) {
 	print $output;
 	# die "looks like $arg is not a nice rrd " unless $output;
 	unless ($output) {
+		# no need to kilroy at STDERR - rrdtool complains there
 		print "\tlooks like $arg is not a nice rrd " ;
 		$errcnt ++ ;
 		next;
 	}
+	
+	my @lines = split ("\n", $output);
+	# print "has $#lines lines \n";
+	if ( $#lines != 2) {
+		# print 
+		print STDERR "\tunexpected output format\n$output\n " ;
+		$errcnt ++ ;
+		next;
+	}
+	
+	# assemble the user friendly output:
+	# printf "== %s == | %s\n", $arg, $lines[0]; # db name in top left, col headers follow
+	
+	unless ( $lines[2] =~ /^(\d{10,}):\s*(.*)$/ ) {
+		print STDERR "\tunexpected second line in output\n$lines[2]\n " ;
+		$errcnt ++ ;
+		next;
+	}
+	# if succesful 'til here, we have the 2nd line splitted in the regexp backrefs	
+	$datetimestr = `date -d \@$1`;
+		chomp $datetimestr ;
+	$restofline = $2;
+	$okstring = 'no clue' ;
 
+	# render the user friendly part
+	printf "== %s == | %s\n", $arg, $lines[0]; # db name in top left, col headers follow
+	printf "?%s? >%s< >%s< \n", $okstring, $datetimestr, $restofline ;
 }
 print " ~ ~ ~ DONE ~ ~ ~ \n\terrors found: $errcnt\n";
