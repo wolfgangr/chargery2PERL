d439695c46231145b7410b82c26dae2005f62be4
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-20 23:57:35 +0100

    preparing system call

--- setup/reset_ttyUSB.pl
+++ setup/reset_ttyUSB.pl
@@ -2,12 +2,18 @@
 #
 # https://askubuntu.com/questions/645/how-do-you-reset-a-usb-device-from-the-command-line
 # compile `usbreset.c`provided there and make it executable, eg in /usr/local/lib
-
+# tested with dongle at one extra hub, not testet at other nesting level
+#
 use warnings;
 use strict;
 use Cwd;
 
 my $dev_link = shift @ARGV or die "usage: $0 devicefile ";
+
+my $usbreset = `which usbreset` or die " cannot find usbreset"; 
+chomp $usbreset ;
+
+# the challenge is to climb down the device tree
 
 my $realpath = Cwd::abs_path($dev_link ); 
 print "\$realpath: $realpath\n";
@@ -18,7 +24,7 @@ print "\$tty_devpath: $tty_devpath\n";
 
 #       E: DEVPATH=/devices/pci0000:00/0000:00:12.0/usb4/4-5/4-5:1.0/ttyUSB1/tty/ttyUSB1
 $tty_devpath =~ /^(E: DEVPATH=)(.+)$/ or die "cannot parse $tty_devpath";
-my $usb_devpath = '/sys' . $2 . '/device/../..';
+my $usb_devpath = '/sys' . $2 . '/device/../..';   # this is the quirk
 print "\$usb_devpath: $usb_devpath\n";
 my $usb_devp_a = Cwd::abs_path($usb_devpath);
 print "\$usb_devp_a: $usb_devp_a\n";
@@ -34,6 +40,6 @@ $usb_devname =~ /^(E: DEVNAME=)(.+)$/ or die "cannot parse $usb_devname";
 my $resettable = $2;
 print "\$resettable: $resettable\n";
 
-
+system (" $usbreset  $resettable ");
 
 
