75b7ed011b829cf2c2762da5db70e32488309c58
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-21 19:24:57 +0100

    evaluate genreal status var

--- render/status.pl
+++ render/status.pl
@@ -14,7 +14,8 @@ use Time::Piece;
 # cells.rrd  pack56.rrd  pack57.rrd
 my $rrd_dir = '../';
 my $rrd_cells = $rrd_dir . 'cells.rrd'; 
-
+my $rrd_p56   = $rrd_dir . 'pack56.rrd';
+my $rrd_p57   = $rrd_dir . 'pack57.rrd';
 
 # reference data from cell data sheet
 our %pack_info;
@@ -39,6 +40,16 @@ my $title = sprintf "BMS status %s V %s Ah %s %s %dS%dP",
 my %cells_state = rrd_lastupdate ( $rrd_cells );
 my $n_cells = scalar @{$cells_state{ds_tags}};
 
+my %p56_state = rrd_lastupdate ( $rrd_p56 );
+my %p57_state = rrd_lastupdate ( $rrd_p57 );
+
+my %tv ;
+for my $tag ( qw (Vtot Ah Wh)) 
+ 	{ $tv{ $tag } = $p56_state{ ds_last }->[ $p56_state{ds_map}->{ $tag } ] ; }
+for my $tag ( qw ( curr mode Vend_c SOC temp1 temp2  )) 
+        { $tv{ $tag } = $p57_state{ ds_last }->[ $p57_state{ds_map}->{ $tag } ] ; }
+
+
 
 my $soc_symbol = '<table cellpadding ="0" cellspacing="0" ><tr>' ;
 
@@ -57,7 +68,7 @@ $soc_symbol .= '</tr></table>';
 #
 print header();
 print start_html(-title => $title);
-print h1($title);
+print h3($title);
 
 print '<hr><table border=\"1\"><tr>'."\n";
 print '<td valign="top"><table><tr>'."\n";
@@ -67,10 +78,29 @@ print '<td >soc</td><td width="300">' . $soc_symbol  .  '</td>'."\n";
 print '</tr></table></td>'."\n";
 print '</tr><tr>';
 
-print '<td><table border="1" >'."\n";
-print '<tr><td>foo</td><td>bar</td></tr>'."\n";
-print '<tr><td>tralala</td><td>pipapo</td></tr>'."\n";
-print '<tr><td>asdf</td><td>jklö</td></tr>'."\n";
+print '<td><table border="0" cellspacing ="3"  cellpadding="5"  bgcolor="#cccccc"  >'."\n";
+
+# print '<tr><td>&nbsp</td> <td>foo</td><td>bar</td></tr>'."\n";
+# print '<tr><td>tralala</td><td>pipapo</td><td>&nbsp</td> </tr>'."\n";
+# print '<tr><td>asdf</td><td>&nbsp</td> <td>jklö</td></tr>'."\n";
+
+my $tv_format = '<tr bgcolor="#ffffff" >'
+	. '<td align="right" >%s:&nbsp;</td>'
+	. '<td align="center" ><b>&nbsp;%s&nbsp;</b></td>' 
+	. '<td>&nbsp;%s&nbsp;</td></tr>'
+	. "\n";
+
+printf $tv_format, 'Betriebszustand' , '###' , '' ;
+printf $tv_format, 'Gesamtspannung' , $tv{Vtot} , 'V' ;
+printf $tv_format, 'Strom', , $tv{curr} , 'A' ;
+printf $tv_format, 'Ladezustand', , $tv{SOC} , '%' ;
+printf $tv_format, 'Ladung', , $tv{Ah} , 'Ah' ;
+printf $tv_format, 'Energie', , $tv{Wh} /1000 , 'kWh' ;
+printf $tv_format, 'Batterietemperatur', , $tv{temp2}  , '°C' ;
+printf $tv_format, 'BMS Temperatur', , $tv{temp1}  , '°C' ;
+
+
+
 
 print '</table></td>'."\n";
 
@@ -100,8 +130,9 @@ print "\$rrd_ERR: $cells_state{ rrd_errstr } \n";
 print "\$lastupdate_hr: $cells_state{ lastupdate_hr } \n";
 
 
-print Dumper ( \%cells_state );
- 
+# print Dumper ( \%cells_state );
+# print Dumper ( \%p56_state , \%p57_state ); 
+print Dumper ( \%tv );
 
 print "</pre>\n";
 # ~~~~~~~~ end of debug ~~~~~~~~~~~~~~~~~~~~~~~~~
