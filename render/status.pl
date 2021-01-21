69ae6858c86559f6cc17447e7f2e3271415cf6e2
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-21 13:28:05 +0100

    retrieve and return map tag=>index and arrays of tags, values and sec-unknown

--- render/status.pl
+++ render/status.pl
@@ -106,7 +106,7 @@ sub rrd_lastupdate {
 	#     ];
 
 	my $rrd_info  = RRDs::info ($rrdfile);
-	$rvh{rrd_info} = $rrd_info ;
+	# $rvh{rrd_info} = $rrd_info ; # for debug we want to know anything
         if (defined ($rvh{'rrd_err' } =  RRDs::error) ) {
                 $rvh{'rrd_errstr' } = $rvh{'rrd_err' };
                 return %rvh ;
@@ -130,21 +130,19 @@ sub rrd_lastupdate {
 	 
 	# map `ds[U01].index = 0`  to % ( label => index ) 
 	my %ds_map = map {  /^ds\[(\S+)\]\.index$/  ?   ( $1 , $$rrd_info{ $_}   ) : ( )  } sort keys %$rrd_info ;
-	# my @ds_tags = map { $ds_map{ $_ } } sort { $a <=> $b }  keys %ds_map; 
-	
-	# tag array - rotec by index
-	my @ds_tags =  sort  {  $ds_map{ $a } <=> $ds_map{ $b }   }   keys %ds_map; 
-	# my @ds_index = map sprintf 
-
-	$rvh{ ds_tags } = \@ds_tags ;
 	$rvh{ ds_map } = \%ds_map ;
 
-	# parse for stuff like
-	# 	 ds[U18].last_ds = "2.049"
+	# tag array - sorted by index
+	my @ds_tags =  sort  {  $ds_map{ $a } <=> $ds_map{ $b }   }   keys %ds_map; 
+	$rvh{ ds_tags } = \@ds_tags ;
 
-	#my $lastupdate_obj = Time::Piece->new($lastupdate);
-	#my $lastupdate_hr = $lastupdate_obj->datetime ;
-	
+	# last updated values as array
+	my @ds_last = map {  $$rrd_info{  sprintf 'ds[%s].last_ds', $_   }    } @ds_tags ;
+	$rvh{ ds_last  } = \@ds_last ;
+
+	my @ds_s_unkn = map {  $$rrd_info{  sprintf 'ds[%s].unknown_sec', $_   }    } @ds_tags ;
+	$rvh{ds_s_unkn} = \@ds_s_unkn ;
+
 	$rvh{'rrd_errstr' } ='-' ;
 	return %rvh ;
 		
