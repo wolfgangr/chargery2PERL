16f604a357ce159a9494ff7c6fcf14f121c8c081
wrosner <wrosner@kellerkind.rosner.lokal>
2021-01-21 10:02:21 +0100

    add cell info for status evalutation

---
+++ BMS-pack-info.pm
@@ -0,0 +1,28 @@
+
+our %pack_info = (
+	n_serial 	=> 22,
+	n_parallel 	=> 10,
+	cell_type	=> 'Yinlong LTO66160H',
+
+	C_nom		=> 40,
+	I_nom		=> 40,
+	I_max_chg	=> 400,
+	I_max_dischg    => 400,
+
+
+	U_max_cutoff	=> 2.9,
+	U_max_stop	=> 2.8,
+	U_max_moderate	=> 2.7,
+	U_max_gentle    => 2.6,
+
+	U_nom           => 2.3,
+
+	U_min_gentle	=> 2.0,
+	U_min_moderate	=> 1.9,
+	U_min_stop	=> 1.8,
+	U_min_cutoff    => 1.5,
+
+)
+
+
+1;
